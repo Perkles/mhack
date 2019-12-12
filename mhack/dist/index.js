adb35b0f548d881663c098f99eeceb892a5e50fa
Perkles <otaviocha@gmail.com>
2019-12-12 00:12:44 -0300

    Add request library

--- mhack/dist/index.js
+++ mhack/dist/index.js
@@ -4,11 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const request = require('request');
 let app = express_1.default();
 const github_base_route = 'https://api.github.com/';
 const app_base_route = '';
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    request('https://api.github.com/users/defunkt', function (error, response, body) {
+        console.log(body);
+    });
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
--- mhack/src/index.ts
+++ mhack/src/index.ts
@@ -1,11 +1,9 @@
 import express from "express";
+
 let app = express();
 
-const github_base_route = 'https://api.github.com/';
-const app_base_route = '';
-
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+
 });
 
 app.listen(3000, function() {
