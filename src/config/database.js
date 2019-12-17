1f28dcfd7a8a9568471cb2a2bd238f97371e123f
Perkles <otaviocha@gmail.com>
2019-12-16 22:45:33 -0300

    Install sequelizer

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'root',
+    databse: 'mhack',
+    define: {
+        timestamps: true,
+        underscored: true,
+        undercoresdAll: true,
+    },
+};
