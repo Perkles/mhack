0a2c09c12a4c195a425e09f85cb569743fb232a3
Perkles <otaviocha@gmail.com>
2019-12-27 23:46:17 -0300

    Github OAuth autentication setup

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
 import Router from 'express';
+const path = require('path');
+const request = require('request');
+
 const routes = new Router();
 
 import UserController from './app/controller/UserController';
@@ -9,5 +12,34 @@ routes.post('/users', UserController.store);
 // routes.get('/', (req, res) => {
 //     return res.json({ message: 'hello word '});
 // });
+
+routes.get('/users', (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+routes.get('/users/signin/callback', (req, res) => {
+    const code = req.query.code;
+    console.log(code);
+
+    // request.js post request
+    const options = {
+        url: 'https://github.com/login/oauth/access_token',
+        json: true,
+        body: {
+            client_id: 'dc885fbf11d3232616bc',
+            client_secret: 'a9d70f98b5bd165cbe45f9b767a9869e25792587',
+            code: code
+        }
+    };
+    
+    request.post(options, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(`Status: ${res.statusCode}`);
+        console.log(body);
+    });
+
+});
 
 export default routes;
