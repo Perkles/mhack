fd6622c88e17992a9b9ce4dccb7e08013075579e
Perkles <otaviocha@gmail.com>
2020-01-06 01:37:34 -0300

    Verify if the user is already registred or not

--- mhack/authentication/authenticate.py
+++ mhack/authentication/authenticate.py
@@ -1,8 +1,24 @@
 import requests, json
+from authentication.models import User
 
+def verify_new_user(token_type, access_token):
+    id = ''
+    headers = {'Authorization': '{} {}'.format(token_type, access_token)}
+    response = requests.get('https://api.github.com/user', headers=headers).json()
 
-def verify_new_user(code_response):
-    headers = {'Authorization':'token ' + code_response}
-    response = requests.get('https://api.github.com/user/', headers=headers)
-    print(response.text)
-    
+    # That could be encapsulated
+    for key, value in response.items():
+        if (key == 'id'):
+            id = value
+
+    try:
+        User.objects.get(pk=id)
+        return True
+    except User.DoesNotExist:
+        return False
+
+def register_new_user():
+    print('new user')
+
+def set_access_token():
+    print('already registred user')
--- mhack/authentication/views.py
+++ mhack/authentication/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from .authenticate import *
-
+from urllib import parse
 
 from authentication.models import User
 from authentication.serializer import UserSerlializer
@@ -23,6 +23,10 @@ class ManualUserRegistration(APIView):
 
 class GithubRegistration(APIView):
     def get(self, request, format=None):
+        # List of things that this URL is in charge of:
+        #     log in with github
+        #     verify if user is already registred
+
         # Callback function to https://github.com/login/oauth/authorize?client_id=dc885fbf11d3232616bc
         # if the user logs in successfully in Github the callback should return a query param named
         # code.
@@ -37,9 +41,23 @@ class GithubRegistration(APIView):
             'client_secret': 'a9d70f98b5bd165cbe45f9b767a9869e25792587',
             'code': code_response
         })
-        print(response.text)
+
+        # Lack refatoration, that code may be in a package 
+        access_token = ''
+        token_type = ''
+        response_dic = parse.parse_qs(parse.urlsplit(response.text).path)
+        for key, value in response_dic.items():
+            if (key == 'access_token'):
+                access_token = str(value).replace('[', '').replace("'", '').replace("]", '')
+            elif (key == 'token_type'):
+                token_type = str(value).replace('[', '').replace("'", '').replace("]", '')
+        # Seriously though
+
         if response.status_code == 200:
-            verify_new_user(code_response)
+            if(verify_new_user(token_type, access_token)):
+                set_access_token()
+            else:
+                register_new_user()
             return Response(status=status.HTTP_201_CREATED)
         else:
             return Response(status=status.HTTP_204_NO_CONTENT)
