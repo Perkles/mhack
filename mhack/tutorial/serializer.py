bb1975ee77ba520353c035d9a7591e9eb4d8861d
Perkles <otaviocha@gmail.com>
2020-01-07 23:47:55 -0300

    remove tutorial folder

--- mhack/tutorial/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from tutorial.models import Test
-
-class TestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Test
-        fields = ['id', 'text']
